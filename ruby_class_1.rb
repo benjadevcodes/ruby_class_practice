653371f222580b42ac8beaf9ef18b3678d82f88a
benjadevcodes <benja.codes@gmail.com>
2026-02-24 17:40:55 -0300

    little modification to the file name

---
+++ ruby_class_1.rb
@@ -0,0 +1,50 @@
+class MyCar
+
+    def initialize(year, color, model) 
+      # States
+      @current_speed = 0
+      puts "-----------Specification-------------"
+      @year = year
+      puts "The year is: #{year}"
+      @color = color
+      puts "The color is: #{color}"
+      @model = model
+      puts "The model is: #{model}"
+      puts "-----------Actions-------------"
+    end
+
+    # Methods 
+    def speed_up(number) # add speed to @current_speed variable
+        @current_speed += number
+        puts "The car is moving at #{number} mph"
+    end
+    
+    def brake(number) # slow down the cars and then stops
+        @current_speed += number
+        puts "The car slowdows to #{number} mph"
+    end
+
+    def current_speed
+      puts "Yor current speed is #{@current_speed} mph"
+    end
+
+    def shut_off
+        @current_speed = 0
+        puts "The car is off"
+    end
+end
+
+mazda = MyCar.new("1995","Red", "Sport")
+
+mazda.speed_up(20)
+mazda.current_speed
+mazda.speed_up(20)
+mazda.current_speed
+
+mazda.brake(20)
+mazda.current_speed
+mazda.brake(20)
+mazda.current_speed
+
+mazda.shut_off
+mazda.current_speed
